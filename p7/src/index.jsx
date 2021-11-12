671965d9af0532541646f95014bfff287f55d960
Kazunori Minoda <kazunori.minoda@gmail.com>
2021-11-11 16:42:03 -0800

    Again

--- p7/src/index.jsx
+++ p7/src/index.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const UserGreeting = (props) => {
-    return <h1>Welcome back!</h1>
+    return <h1>Welcome back!</h1>;
 }
 
 const GuestGreeting = (props) => {
-    return <h1>Please sign up.</h1>
+    return <h1>Please sign up.</h1>;
 }
 
 const Greeting = (props) => {
@@ -39,26 +39,31 @@ class LoginControl extends React.Component{
     constructor(props){
         super(props);
         this.handleLogicClick = this.handleLoginClick.bind(this);
-        this.handleLogoutClick.bind = this.handleLogoutClick.bind(this);
+        this.handleLogoutClick = this.handleLogoutClick.bind(this);
         this.state = {isLoggedIn: false};
+        console.log('this.state.isLoggedIn: '+this.state.isLoggedIn);
     }
 
-    handleLoginClick(){
-        this.setState({isLoggedin: true});
+    handleLoginClick = () => {
+        this.setState({isLoggedIn: true});
+        console.log('Called handleLoginClick');
     } 
 
-    handleLogoutClick(){
-        this.setState({isLoggedin: false});
+    handleLogoutClick = () => {
+        this.setState({isLoggedIn: false});
+        console.log('Called handleLogoutClick.');
     }
 
     render(){
         const isLoggedIn = this.state.isLoggedIn;
+        console.log('render -> isLoggedIn: '+ isLoggedIn);
         let button;
+
         if(isLoggedIn){
             button = <LogoutButton onClick={this.handleLogoutClick} />;
         }
         else{
-            button = <LoginButton onClick={this.handleLoginClick()} />;
+            button = <LoginButton onClick={this.handleLoginClick} />;
         }
 
         return (
@@ -66,8 +71,8 @@ class LoginControl extends React.Component{
                 <Greeting isLoggedIn={isLoggedIn} />
                 {button}
             </div>
-        )
-    };
+        );
+    }
 }
 
 ReactDOM.render(
