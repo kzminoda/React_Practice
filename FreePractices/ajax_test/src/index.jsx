bd270475da85de774d5ecd1322623926b1f4ee78
Kazunori Minoda <kazunori.minoda@gmail.com>
2021-11-29 08:57:12 -0800

    2021.11.29

---
+++ FreePractices/ajax_test/src/index.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class DisplayData extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: [],
+        };
+    }
+
+    componentDidMount = () => {
+        fetch("http://10.78.102.84/ofts/tools/test/count.php")
+        .then(res => res.json())
+        .then(
+            (result) => {
+                console.log(JSON.stringify(result));
+                this.setState({
+                    isLoaded: true,
+                    items: result,
+                });
+            },
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error
+                });
+            }
+        );
+    }
+
+    render(){
+        const {error, isLoaded, items} = this.state;
+        
+        if(error){
+            return <div>Error: {error.message}</div>
+        }
+        else if(!isLoaded){
+            return <div>Now Loading...</div>
+        }
+        else{
+            return (
+                <table border={1}>
+                    <thead>
+                    <tr>
+                        <th>Handler</th>
+                        <th>件数</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {items.map((item) => (
+                            <>
+                            <tr>
+                            <td key={item.Handler}>{item.Handler}</td>
+                            <td>{item.Count.toString()}</td>
+                            </tr>
+                            </>
+                        ))}
+                    </tbody>
+                </table>
+            );
+        }
+    }
+}
+
+ReactDOM.render(
+    <DisplayData />,
+    document.getElementById('root')
+);
